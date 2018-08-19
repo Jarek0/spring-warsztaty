fd005756d87e107e8cf414fef633ebd06b17892e
Jarek <jaroslaw.bielec@pollub.edu.pl>
2018-08-19 14:27:33 +0200

    secondary table

--- src/main/java/pl/edu/pollub/warsztaty/userAccount/domain/UserAccountEntity.java
+++ src/main/java/pl/edu/pollub/warsztaty/userAccount/domain/UserAccountEntity.java
@@ -16,6 +16,11 @@ import static javax.persistence.GenerationType.IDENTITY;
 @AllArgsConstructor
 @Builder
 @EqualsAndHashCode(of = {"pesel"})
+@SecondaryTables({
+        @SecondaryTable(name = "personal_data", pkJoinColumns = {
+                @PrimaryKeyJoinColumn(name = "user_account_id", referencedColumnName = "id")
+        })
+})
 public class UserAccountEntity {
 
     @Id
@@ -25,21 +30,23 @@ public class UserAccountEntity {
     @Column(nullable = false, unique = true, length = 30)
     private String login;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true, table = "personal_data")
     private String email;
 
-    @Column(nullable = false, length = 30)
+    @Column(nullable = false, length = 30, table = "personal_data")
     private String firstName;
 
-    @Column(nullable = false, length = 30)
+    @Column(nullable = false, length = 30, table = "personal_data")
     private String surname;
 
     @Enumerated(value = STRING)
+    @Column(table = "personal_data")
     private Gender gender;
 
+    @Column(table = "personal_data")
     private LocalDate birthDate;
 
-    @Column(nullable = false, unique = true, length = 11)
+    @Column(nullable = false, unique = true, length = 11, table = "personal_data")
     private String pesel;
 
     private Address homeAddress;
