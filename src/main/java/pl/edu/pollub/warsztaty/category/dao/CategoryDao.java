ae66e66f4a7f726e07821e3a6663c2b5e2ded172
Jarek <jaroslaw.bielec@pollub.edu.pl>
2018-09-05 22:39:12 +0200

    usuniecie category

--- src/main/java/pl/edu/pollub/warsztaty/category/dao/CategoryDao.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.pollub.warsztaty.category.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.edu.pollub.warsztaty.category.domain.CategoryEntity;
-
-@Repository
-public interface CategoryDao extends JpaRepository<CategoryEntity, Long> {
-}
